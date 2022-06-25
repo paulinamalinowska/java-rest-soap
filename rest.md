e7d780e2046ea19d3e4fdd19cf1b1111f54452d3
Paulina.Malinowska <malinowska.contact@gmail.com>
2022-06-25 14:06:52 +0200

    [add] -Added post method

--- rest.md
+++ rest.md
@@ -7,7 +7,10 @@
 - resources are plural!!!
 - Read collection - GET - /stamps
 - Read one item - GET - /stamps/{id} eg. /stamps/110101010
+- result : 200 - if found,404 if not found(empty body) 
 - Create item - POST - /stamps
+  - result: 201 + content(inside body) - positive case
+  - negative case: 400 bad request(no body/errors in response)
 - Delete item - DELETE - /stamps/{id} eg. /stamps/111145
 - Update item - PUT - replace object - /stamps/{id}
 - Update partial - PATCH - partial update - /stamps/{id}
--- src/main/java/pl/sapiens/javarestsoap/controller/RestReservationController.java
+++ src/main/java/pl/sapiens/javarestsoap/controller/RestReservationController.java
@@ -3,36 +3,59 @@ package pl.sapiens.javarestsoap.controller;
 import lombok.extern.slf4j.Slf4j;
 import pl.sapiens.javarestsoap.entity.Reservation;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.util.List;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Slf4j
 @Path("/reservations")
 public class RestReservationController {
 
+
+    private static Reservation theOnlyOne = new Reservation(0L, "Kowalski", 1, LocalDateTime.now(), LocalDateTime.now().plusHours(2), "Wojska Polskiego", "Przy oknie");
+
+
     @GET
-    public Response getReservations(){
-
-        var dummyReservations = List.of(
-                new Reservation(1L ,
-                        "Malinowska",
-                        2,
-                        LocalDateTime.now(),
-                        LocalDateTime.now().plusHours(2),
-                        "Main Station",
-                        "Przy oknie")
+    public Response getReservation() {
+        var dummyReservation = List.of(
+                theOnlyOne
         );
+        log.info("getting all reservation");
+        log.info("number of found reservation : [{}]", dummyReservation.size());
+        return Response.ok(dummyReservation).build();
+    }
 
-        log.info("getting all reservations");
+    @GET
+    @Path("/{id}")
+    public Response findReservationById(@PathParam("id") Long reservationsId) {
+        log.info("trying to find reservation by id: [{}]", reservationsId);
+        // TODO: replace with integration with service
 
-        log.info("number of found reservations: [{}]",dummyReservations.size());
+        Response result;
+        if(reservationsId==1L) {
+            result = Response.ok(theOnlyOne).build();
+        } else {
+            result = Response.status(NOT_FOUND).build();
+        }
+        return result;
+    }
 
-        return Response.ok(dummyReservations).build();
+    @POST
+    public Response createReservation(Reservation toCreate) throws URISyntaxException {
+        log.info("tryging to create reservation: [{}]", toCreate);
+        // TODO: use service, add validation
+        URI location = new URI("/reservations/2");
+        return Response.created(location).build();
 
 
     }
+
 
 }
